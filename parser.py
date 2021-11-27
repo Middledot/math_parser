1acb5beb7e5c7bbe8a71e13cf15b606384137823
Middledot <78228142+Middledot@users.noreply.github.com>
2021-11-27 10:09:02 -0500

    New Code

--- parser.py
+++ parser.py
@@ -1,72 +1,64 @@
-
 import re
+from string import digits
 
-def _process_small_eq(op, num1, num2):
+def _resolve_small_eq(op, num1, num2):
     if op == "+":
-        return num1+num2
+        return float(num1)+float(num2)
     elif op == "-":
-        return num1-num2
+        return float(num1)-float(num2)
     elif op == "*":
-        return num1*num2
+        return float(num1)*float(num2)
     elif op == "/":
-        return num1/num2
+        return float(num1)/float(num2)
     elif op == "**":
-        return num1**num2
+        return float(num1)**float(num2)
     elif op == "^":
-        return num1**num2
+        return float(num1)**float(num2)
     #elif op == "":
     #    return num1+num2
 
 def resolve_expr(exp):
-    _ops = [["^", "ergs"], ["*", "/"], ["+", "-"]]# "%"]
+    ops = [
+        r"(-{0,1}[0-9.]+\*\*-{0,1}[0-9.]+|-{0,1}[0-9.]+\^-{0,1}[0-9.-]+)",
+        r"(-{0,1}[0-9.]+\*-{0,1}[0-9.]+|-{0,1}[0-9.]+\/-{0,1}[0-9.-]+)",
+        r"(-{0,1}[0-9.]+\+-{0,1}[0-9.]+|-{0,1}[0-9.]+\--{0,1}[0-9.-]+)",
+    ]
+    results = []
     exp = exp.replace(" ", "")
-    n_exp = exp
+    p = 0
+    current = []
     for c, i in enumerate(list(exp)):
         if i == "(":
-            p = 0
-            for ce, ie in enumerate(exp[c:]):
-                if ie == "(":
-                    p += 1
-                elif ie == ")":
-                    p -= 1
-                if p == 0:
-                    n_exp = n_exp.replace(exp[c:][:ce+1], resolve_expr(exp[c+1:][:ce-1]))
-                    continue
+            p += 1
+            if len(current) == 0:
+                current.append(c)
+        if i == ")":
+            p -= 1
+            if len(current) == 1:
+                current.append(c)
+            if p == 0 and len(current) == 2:
+                results.append(current)
+                current = []
 
-    ops = _ops.copy()
+    replacables = {f"({exp[i[0]+1:i[1]]})": exp[i[0]+1:i[1]] for i in results}
+    
+    for k, i in replacables.items():
+        exp = exp.replace(k, resolve_expr(i))
 
-    for c, o in enumerate(_ops):
-        amt = 0
-        for i in o:
-            if i not in n_exp:
-                ops[c].remove(i)
-                print(ops[c])
-                amt -= 1
-                
-    print("final: ", ops)
-    for o in ops:
-        print(o)
-        for i in o:
-            print(i)
-            before = n_exp
-            c = 0
-            while True:
-                c += 1
-                res = re.search(f"([0-9.]+\{i}[0-9.]+)", n_exp)
-                if res != None:
-                    res = res.group()
-                    print(res)
-                    x, y = res.split(i)
-                    x, y = int(x), int(y)
-                    simpl = str(_process_small_eq(i, x, y))
-                    print("simplified: ", simpl)
-                    n_exp = n_exp.replace(res, simpl)
-                if n_exp == before:
-                    break
-                else:
-                    before = n_exp
+    results = {}
+    for i in ops:
+        while True:
+            eq = re.search(i, exp)
+            if eq == None:
+                break
+            if eq != None:
+                eq = eq.group()
+                op = eq.translate(eq.maketrans("", "", digits)).strip(".")
+                if len(op) > 1:
+                    op = op[0]
+                results[eq] = _resolve_small_eq(op, *eq.split(op))
 
-    return n_exp
+            for k, r in results.items():
+                exp = exp.replace(k, str(r))
 
-print("answer: ", resolve_expr("3+0.7142857142857143*-4"))
-#print("answer: ", resolve_expr("3+ (5/ 7*2-6)"))
+    return float(exp)
